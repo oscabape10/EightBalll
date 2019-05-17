7963e05916b1c5d4bda4e14db39e9db3bb10fd15
Oscar J Abarca Pena <45542113+oscabape10@users.noreply.github.com>
2019-05-17 21:21:29 +0100

    Update Eight Ball .js

--- Eight Ball .js
+++ Eight Ball .js
@@ -1,14 +1,16 @@
-
-
-let userName = 'Pepe';
+//Create a variable.
+let userName = 'Oscar';
+//ternary expression that decides what to do if the user enters a name or not.
 userName ? console.log(`Hello, ${userName}!.`) : console.log('Hello');
-
+//Variable named userQuestion. wiht question the user would like to ask. 
 let userQuestion = 'Can you asnwer my question?';
+//console log for the question.
 console.log(`The user asked: ${userQuestion}`);
-
+//generate a random number between 0 and 7.
 let randomNumber = 
     Math.floor(Math.random() * 8);
-let eightBall = 0 ;
+//control flow that takes in the randomNumber. Create a if/else switch statements.
+let eightBall = 0;
 switch (randomNumber) { 
   case 0:
     eightBall = 'It is certain';
@@ -35,4 +37,6 @@ switch (randomNumber) {
     eightBall = 'Signs point to yes';
     break;
 }
+//console.log the magical eight ball.
     console.log(eightBall);
+
